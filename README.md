9cb6f9f4892ed0ac060b27a01d688b4573b1f707
sng2c <sng2nara@gmail.com>
2013-06-20 16:37:25 +0900

    fix sample code.

--- README.md
+++ README.md
@@ -8,21 +8,32 @@ version 0.001
 
 # SYNOPSIS
 
+	use lib './lib';
 	use Parse::Token::Simple;
 
 	my @rules = (
-		[ NUMBER => qr/[\d,\.]+/ ],
-		[ WORD => qr/\w+/ ],
-		[ WHITESPACE => qr/\s+/ ],
-		[ NOTMATCH => qr/.*/ ],
+		[ NUM => qr/\d[\d,\.]*/ ],
+		[ STR => qr/\w+/ ],
+		[ SPC => qr/\s+/ ],
+		[ ERR => qr/.*/ ],
 	);
 
-	my $parser = Parse::Token::Simple(rules=>\@rules);
+	my $parser = Parse::Token::Simple->new(rules=>\@rules);
 	$parser->from("This costs 1,000won.");
 	while( ! $parser->eof ){
 		my($state_tag, $token) = $parser->nextToken;
 		print "$state_tag -->$token<--\n";
 	}
+
+Results are
+
+	STR -->This<--
+	SPC --> <--
+	STR -->costs<--
+	SPC --> <--
+	NUM -->1,000<--
+	STR -->won<--
+	ERR -->.<--
 
 # AUTHOR
 
--- lib/Parse/Token/Simple.pm
+++ lib/Parse/Token/Simple.pm
@@ -9,22 +9,33 @@ use Moo;
 
 =head1 SYNOPSIS
 
+	use lib './lib';
 	use Parse::Token::Simple;
 
 	my @rules = (
-		[ NUMBER => qr/[\d,\.]+/ ],
-		[ WORD => qr/\w+/ ],
-		[ WHITESPACE => qr/\s+/ ],
-		[ NOTMATCH => qr/.*/ ],
+		[ NUM => qr/\d[\d,\.]*/ ],
+		[ STR => qr/\w+/ ],
+		[ SPC => qr/\s+/ ],
+		[ ERR => qr/.*/ ],
 	);
 
-	my $parser = Parse::Token::Simple(rules=>\@rules);
+	my $parser = Parse::Token::Simple->new(rules=>\@rules);
 	$parser->from("This costs 1,000won.");
 	while( ! $parser->eof ){
 		my($state_tag, $token) = $parser->nextToken;
 		print "$state_tag -->$token<--\n";
 	}
-	
+
+Results are
+
+	STR -->This<--
+	SPC --> <--
+	STR -->costs<--
+	SPC --> <--
+	NUM -->1,000<--
+	STR -->won<--
+	ERR -->.<--
+
 
 =cut
 
